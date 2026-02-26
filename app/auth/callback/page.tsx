5479d76c0417011f8681a5a0c8f197d6ae26e210
Morikawa <yorozu.tantei999@gmail.com>
2026-02-26 22:12:10 +0900

    fix supabase import

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../../lib/supabase";
 import { useRouter } from "next/navigation";
 
 export default function AuthCallbackPage() {
